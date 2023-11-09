2ea9d0a527a44eda338248617e388361a4306fec
RajandraHS <142873321+RajandraHS@users.noreply.github.com>
2023-11-09 11:43:06 +0700

    Add files via upload

---
+++ Kamis, 14 September, 2022/tugas1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("+++++++ TUGAS NOMOR 1, PERKENALAN MAHASISWA +++++++\n\n");
+	
+	int NIM = 2143014;
+    char NAMA []= "Rajandra Hanif Syahputra";
+    char JURUSAN []= "D3 teknik Informatika";
+
+    printf("Perkenalkan, Nama saya %s, Dengan NIM: %d, Saya adalah Mahasiswa STT Mandala dari Jurusan: %s\n",NAMA, NIM, JURUSAN);
+
+    return 0;
+}
